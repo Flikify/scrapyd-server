4670c21df81eb9b536aae11f37744a3068431be9
Flik <2470982985@qq.com>
2024-11-02 14:53:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# 确保目录存在
-mkdir -p "$DIR/config" "$DIR/scrapyd"/{eggs,items,logs,dbs}
-
 docker run -d --name=test \
   -p 6800:6800 \
   -p 5000:5000 \
